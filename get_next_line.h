457fc3dd9b204a32a7d84b929e3c6135111a9d60
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-10-29 11:32:12 -0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -7,11 +7,11 @@
 #  define BUFFER_SIZE 10
 # endif
 
-  char *get_next_line(int fd);
-  char *ft_strchr(const char *str, int argument);
-  size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
-  char	*ft_substr(char const *s, unsigned int start, size_t len);
-  char	*ft_strjoin(const char *s1, const char *s2);
-  char	*ft_strdup(char *src);
+  char    *get_next_line(int fd);
+  char    *gnl_strchr(const char *str, int argument);
+  size_t  gnl_strlcpy(char *dest, const char *src, size_t destsize);
+  char    *gnl_substr(char const *s, unsigned int start, size_t len);
+  char    *gnl_strjoin(const char *s1, const char *s2);
+  char    *gnl_strdup(char *src);
 
 #endif
